46a3c28c89a1ace81156ac25eea47954a39b32e7
John <ullrickjojn@gmail.com>
2023-12-04 19:11:32 +0500

    feat created game in javascript

---
+++ main.js
@@ -0,0 +1,33 @@
+// let text = '';
+
+// for(let i = 0; i < 10; i++){
+
+// }
+
+let luckyNum = 7
+
+while (true){
+    let answ = +prompt("O'ylangan sonni kiriting")
+    if(answ === luckyNum){
+        alert('Siz yutdingiz')
+        break;
+    }
+    else if(answ == 0){
+        alert('Son kiritilmadi!')
+        break;
+    }    
+    else if (isNaN(answ)){
+        alert('Faqat son kiriting! Harf kiritish mumkin emas ')
+    }
+    else if (answ > 10){
+        alert('1 dan 10 gacha son kiriting! 10 dan katta son kiritish mumkin emas!')
+    }
+    else if (answ < 0){
+        alert('1 dan 10 gacha son kiriting! 0 dan kichik sonlar mumkun emas')
+
+    }
+    else{
+        alert('siz topa olmadingiz')
+    }
+
+}
